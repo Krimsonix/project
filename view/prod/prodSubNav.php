f1e08c393a87cb6ac18647829c97ff6904930b25
krimsonix <krimsonix@yahoo.com>
2013-05-28 22:02:37 -0700

    Added Product Page

---
+++ view/prod/prodSubNav.php
@@ -0,0 +1,14 @@
+
+      <!-- sub navigation -->
+      <div class="span3">
+        <div class="well sidebar-nav">
+          <ul class="nav nav-list">
+            <li class="nav-header">Products Menu</li>
+            <li><a href="index.php?q=prod&a=list">List All Products</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+          </ul>
+        </div><!--/.well -->
+      </div><!--/span-->
+      <!-- end sub navigation -->
